72e371b6d74e98290f367ed36ae7e1775943acb7
YagneswarReddy <164169551+YagneswarReddy@users.noreply.github.com>
2024-12-07 09:07:44 +0530

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,5 @@
+INSERT INTO books (book_id, title, author, genre, price, published_year) 
+VALUES (1, 'Spring Boot in Action', 'Craig Walls', 'Technology', 45.99, 2016);
+
+INSERT INTO books (book_id, title, author, genre, price, published_year) 
+VALUES (2, 'Clean Code', 'Robert Martin', 'Technology', 50.00, 2008);
